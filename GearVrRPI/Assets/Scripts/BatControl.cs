3baf2ee0e4c8d7929a250a5c7f10025e5202ab75
John Conover <111jbcon@gmail.com>
2015-11-14 16:58:05 -0500

    Basic movement and scripts

---
+++ GearVrRPI/Assets/Scripts/BatControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatControl : MonoBehaviour {
+    
+    public float movementSpeed = 10f;
+
+    void Awake()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    void ProcessInput()
+    {
+        float movement = Input.GetAxis("Vertical");
+        transform.position += transform.forward * movement * movementSpeed * Time.deltaTime;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
