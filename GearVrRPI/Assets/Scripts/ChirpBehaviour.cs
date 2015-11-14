69637ff090ecbfc535fb9ba59ce25d5ef78c20af
John Conover <111jbcon@gmail.com>
2015-11-14 18:32:00 -0500

    Fixed merge conflict with branch 'flying'

---
+++ GearVrRPI/Assets/Scripts/ChirpBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChirpBehaviour : MonoBehaviour {
+    //initialize vars
+    public Rigidbody chirp;
+    public float force = 20f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("AButton"))
+        {
+            Rigidbody newChirp = Instantiate(chirp, 
+                transform.position, transform.rotation) as Rigidbody;
+
+            newChirp.AddForce(newChirp.transform.forward * force);
+        }
+	}
+}
